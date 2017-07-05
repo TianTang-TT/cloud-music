769c202b90870cdd6c95db0567d501c3809ff0cf
honor <honor_loyalty@126.com>
2017-07-05 17:05:53 +0800

    init vuex

--- config/index.js
+++ config/index.js
@@ -23,8 +23,8 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 8080,
-    autoOpenBrowser: true,
+    port: 9000,
+    autoOpenBrowser: false,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {},
