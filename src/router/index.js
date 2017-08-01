3fe05ea106daa371bf9a464399bca2c792008329
honor <honor_loyalty@126.com>
2017-08-01 13:55:50 +0800

    separate routes from router

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
-import Home from 'view/home'
-import Play from 'view/play'
-
-import Recommend from 'view/home/recommend'
-const Hot = () => import('view/home/hot')
-const Search = () => import('view/home/search')
+import routes from './routes'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-      redirect: '/recommend',
-      children: [
-        {path: 'recommend', name: 'recommend', component: Recommend, meta: {name: '推荐音乐', index: 1, cat: 'home'}},
-        {path: 'hot', name: 'hot', component: Hot, meta: {name: '热歌榜', index: 2, cat: 'home'}},
-        {path: 'search', name: 'search', component: Search, meta: {name: '搜索', index: 3, cat: 'home'}}
-      ]
-    },
-    {
-      path: '/play',
-      name: 'play',
-      component: Play
-    }
-  ]
+const router = new Router({
+  routes
 })
+
+export default router
---
+++ src/view/Welcome.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="welcome">
+    <p class="welcome-text">时光时光慢些吧...</p>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'welcome'
+  }
+</script>
+<style rel="stylesheet/scss" lang="scss" scoped>
+  .welcome {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: #d43c33;
+    .welcome-text {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      color: #fff;
+      @include fontpx(40);
+    }
+  }
+</style>
