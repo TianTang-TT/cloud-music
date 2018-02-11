dad2634f7357af019cff4caaf204709c0e8bf247
Lancer <honor_loyalty@126.com>
2018-02-11 16:29:07 +0800

    修改语法bug

--- client/build/webpack.prod.conf.js
+++ client/build/webpack.prod.conf.js
@@ -2,6 +2,7 @@ const path = require('path')
 const merge = require('webpack-merge')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const baseWebpackConfig = require('./webpack.base.conf')
 const config = require('../config')
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,5 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-const test = async function() {
-  let result = await fetch('test')
-  return result
-}
 
 ReactDom.render(
   <h1>hello world</h1>,
