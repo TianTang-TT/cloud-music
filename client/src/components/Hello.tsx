b7c4065dea3244c03b49b4c9cfc3631c9ed15b88
heaven <honor_loyalty@126.com>
2018-02-21 16:51:28 +0800

    update

--- client/src/components/Hello.tsx
+++ client/src/components/Hello.tsx
@@ -6,6 +6,9 @@ export interface HelloProps { compiler: string; framework: string; }
 // State is never set so we use the '{}' type.
 export class Hello extends React.Component<HelloProps, {}> {
     render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+        return <h1>
+            Hello from {this.props.compiler} and {this.props.framework}!
+            it's a try for ts and react!
+        </h1>;
     }
 }
