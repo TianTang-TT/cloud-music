f035bd763e7e902325c84fe82cc198037ce115fd
Lancer <honor_loyalty@126.com>
2018-03-06 16:43:40 +0800

    初始化fetch文件

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,1 +1,15 @@
 // 创建请求对象，在此对请求进行拦截和处理
+import axios from 'axios'
+
+const service = axios.create({
+    baseURL: '',
+    timeout: 5000
+})
+
+service.interceptors.request.use(option => {
+    return option
+}, error => Promise.reject(error))
+
+service.interceptors.response.use(response => {
+    return response
+}, error => Promise.reject(error))
