4a9dd6178621020d4b01773fa3bfa282aa31c078
Lancer <honor_loyalty@126.com>
2018-03-09 10:36:29 +0800

    完成header

--- client/src/view/home/Header.tsx
+++ client/src/view/home/Header.tsx
@@ -3,7 +3,14 @@ export class Header extends React.Component<{}, {}> {
     render () {
         return (
             <footer className="cm-header">
-                <div className="wrap"></div>
+                <div className="wrap">
+                    <span className="title">网易云音乐</span>
+                    <div className="download">
+                        <span className="download-text">
+                            下载APP
+                        </span>
+                    </div>
+                </div>
             </footer>
         )
     }
